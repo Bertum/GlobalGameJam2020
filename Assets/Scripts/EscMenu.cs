2d87534d37e5dbf66df0d154b9e78b4d3c0ad5b6
rubenrj <ruben.rj92@gmail.com>
2020-02-01 11:28:08 +0100

    Some menus prefabs and first code

---
+++ Assets/Scripts/EscMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoReset()
+    {
+        //TODO: Reset the game
+    }
+    
+    public void GoMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
