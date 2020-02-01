960cb33d94c0fcd61185fb31b27ed0f10df4f13d
Alberto <albertodelcru@gmail.com>
2020-02-01 13:36:44 +0100

    Water slider and project name

---
+++ Assets/Scripts/Game/WaterMeter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WaterMeter : MonoBehaviour
+{
+    public Slider WaterSlider;
+
+    public void SetWaterLevel(float level)
+    {
+        WaterSlider.value = level;
+    }
+}
