9ddef1b15c890654f22449b0493600278a86d3c2
Alberto <albertodelcru@gmail.com>
2020-02-01 10:34:36 +0100

    Pipes

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public Sprite FixedSprite;
+    public Sprite BrokenSprite;
+    [HideInInspector]
+    public bool IsBroken;
+    private SpriteRenderer spriteRenderer;
+    private PipesManager pipesManager;
+
+    private void Awake()
+    {
+        IsBroken = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        pipesManager = GameObject.FindObjectOfType<PipesManager>();
+    }
+
+    public void BreakPipe()
+    {
+        IsBroken = true;
+        spriteRenderer.sprite = BrokenSprite;
+        pipesManager.CheckBrokenPipes();
+    }
+
+    public void FixPipe()
+    {
+        IsBroken = false;
+        spriteRenderer.sprite = FixedSprite;
+        pipesManager.CheckBrokenPipes();
+    }
+}
--- Assets/Scripts/PipesManager.cs
+++ Assets/Scripts/PipesManager.cs
@@ -1,18 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PipesManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<Pipe> pipes;
+    private int brokenPipes;
+    public int TimeToBreak = 3;
+    private float counter;
+    private int rndToBreak;
+    private bool brokePipe;
+
+    private void Awake()
     {
-        
+        TimeToBreak = 3;
+        brokePipe = false;
+        pipes = new List<Pipe>();
+        var pipesObjects = GameObject.FindGameObjectsWithTag("Pipe");
+        foreach (var pipe in pipesObjects)
+        {
+            pipes.Add(pipe.GetComponent<Pipe>());
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        counter += Time.deltaTime;
+        if (counter >= TimeToBreak)
+        {
+            BreakRandomPipe();
+            counter = 0;
+        }
+    }
+
+    private void BreakRandomPipe()
+    {
+        while (pipes.Any(a => !a.IsBroken) && !brokePipe)
+        {
+            rndToBreak = Random.Range(0, pipes.Count);
+            brokePipe = BreakPipe(rndToBreak);
+        }
+        brokePipe = false;
+    }
+
+    private bool BreakPipe(int index)
+    {
+        var pipeToBreak = pipes[index];
+        if (!pipeToBreak.IsBroken)
+        {
+            pipes[index].BreakPipe();
+            return true;
+        }
+        return false;
+    }
+
+    public void CheckBrokenPipes()
+    {
+        brokenPipes = pipes.Count(c => c.IsBroken);
     }
 }
