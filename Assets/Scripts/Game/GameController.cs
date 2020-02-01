5fcf74ee0b04737cdec6cc7e340e9b4e1a4d887d
Alberto <albertodelcru@gmail.com>
2020-02-01 12:52:58 +0100

    Time player counter

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private bool stopped;
+    private float timePlayed;
+
+    private void Awake()
+    {
+        stopped = false;
+    }
+
+    void Update()
+    {
+        //TODO: change to use the input manager
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            stopped = !stopped;
+            if (stopped)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
+        }
+        timePlayed += Time.deltaTime;
+    }
+}
