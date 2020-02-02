854bdb163fec3eedc3ee782b4f7d959a155eab99
Alberto <albertodelcru@gmail.com>
2020-02-02 09:51:13 +0100

    EndGame trigger

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
     public GameObject character;
     private Transform cameraTransform;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +14,8 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (character.transform.position.y > 2.60 && character.transform.position.y < 48) {
+        if (character.transform.position.y > 2.60 && character.transform.position.y < 48)
+        {
             cameraTransform.position = new Vector3(0, character.transform.position.y, cameraTransform.position.z);
         }
     }
